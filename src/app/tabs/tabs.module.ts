6cdc53d37db2ad40300ab032d711aa117c99e346
Francesco <fleuzzi97@gmail.com>
2023-01-04 18:05:47 +0100

    start con tabsmenu, material, ionic

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -6,13 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
 import { TabsPage } from './tabs.page';
-
+import {MatTableModule} from '@angular/material/table';
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
+    TabsPageRoutingModule,
+    MatTableModule
   ],
   declarations: [TabsPage]
 })
