5b81c93ba6d62cdecf5bae422fd989e92dad4af6
Francesco <fleuzzi97@gmail.com>
2023-01-05 12:30:18 +0100

    creazione dei modelli necessari alla gestione dei tre file.json

---
+++ src/app/models/IGroups.ts
@@ -0,0 +1,6 @@
+export interface IGroups {
+  id: number;
+  name: string;
+  description: string;
+  role: number;
+}
---
+++ src/app/models/IRoles.ts
@@ -0,0 +1,6 @@
+export interface IRoles {
+  id: number;
+  title: string;
+  description: string;
+  default: boolean;
+}
